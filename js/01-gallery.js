e13ff66c38dbc091b2cdb75faabc0983c032a4f0
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2023-07-04 01:10:28 +0300

    Зробив 1 завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const imagesListArr = galleryItems
+  .map((image, index) => {
+    return `
+<li class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+        <img
+        class="gallery__image"
+        src="${image.preview}"
+        data-source="${image.original}"
+        alt="${image.description}"
+        />
+    </a>
+</li>`;
+  })
+  .join("");
+
+const imageslistEl = document.querySelector(".gallery");
+
+imageslistEl.insertAdjacentHTML("beforeend", imagesListArr);
+
+document.querySelector("ul.gallery").onclick = (event) => {
+  event.preventDefault();
+  console.log(event.target.dataset.source);
+  if (event.target.tagName === "IMG") {
+    basicLightbox
+      .create(
+        `
+		<img width="1250" height="850" src="${event.target.dataset.source}">
+	`
+      )
+      .show(basicLightbox);
+    window.addEventListener("keydown", onEscKeyPress);
+    function onEscKeyPress(event) {
+      if (event.code === "Escape") {
+        close((basicLightbox) => {
+          window.removeEventListener("keydown", onEscKeyPress);
+        });
+      }
+    }
+  }
+};
